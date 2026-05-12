e67ee57b633e5820649e1b25ca56c794a26cd1b9
Bipin Ghimire <gbipin042@gmail.com>
2026-05-12 08:16:48 +0545

    arithmetic operator

---
+++ operator/operator.js
@@ -0,0 +1,59 @@
+ 
+ /* function sum() {
+            let num1 = Number(document.getElementById("num1").value);
+            let num2 = Number(document.getElementById("num2").value);
+
+            document.getElementById("result").innerHTML =
+                "Sum = " + (num1 + num2);
+        }
+
+        function subtract() {
+            let num1 = Number(document.getElementById("num1").value);
+            let num2 = Number(document.getElementById("num2").value);
+
+            document.getElementById("result").innerHTML =
+                "Subtract = " + (num1 - num2);
+        }
+
+        function multiply() {
+            let num1 = Number(document.getElementById("num1").value);
+            let num2 = Number(document.getElementById("num2").value);
+
+            document.getElementById("result").innerHTML =
+                "Multiply = " + (num1 * num2);
+        }
+
+        function divide() {
+            let num1 = Number(document.getElementById("num1").value);
+            let num2 = Number(document.getElementById("num2").value);
+
+            document.getElementById("result").innerHTML =
+                "Divide = " + (num1 / num2);
+        }
+*/
+
+function arithemticCalculator(operation){
+    let num1= Number(document.getElementById("num1").value);
+    let num2= Number(document.getElementById("num2").value);
+
+    let result;
+    
+    if(operation=="sum"){
+        result=num1+num2;
+    }
+
+    else if(operation=="sub"){
+        result=num1-num2;
+    }
+
+    else if(operation=="mul"){
+        result=num1*num2;
+    }
+
+    else if (operation=="div"){
+        result=num1/num2;
+    }
+
+    document.getElementById("result").innerHTML="Result=" + result;
+
+}
