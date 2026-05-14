02195c281e834f3566cc14460e32833cdd17e9df
Bipin Ghimire <gbipin042@gmail.com>
2026-05-14 18:02:43 +0545

    bankingsystem

---
+++ bankingsystem/script.js
@@ -0,0 +1,72 @@
+
+let balance = 10000;
+const pin = "1234";
+
+
+function withdraw() {
+
+    let amount = Number(document.getElementById("amt").value);
+
+    // Checking valid amount
+    if (amount <= 0) {
+        document.getElementById("invalid").innerText= "Invalid Amount";
+        return;
+    }
+
+    // Checking multiple of 100
+    if (amount % 100 !== 0) {
+        document.getElementById("invalid").innerText= "Amount must be multiple of 100";
+        return;
+    }
+
+    // Checking sufficient balance
+    if (amount > balance) {
+        document.getElementById("invalid").innerText= "Insufficient Balance";
+        return;
+    }
+
+    // Asking for pin
+    let enterPin = prompt("Enter PIN");
+
+    if (enterPin == pin) {
+        balance = balance - amount;
+        document.getElementById("totalamount").innerText = "Total Balance : " + balance;
+        alert("Withdrawal Successful");
+    } 
+    
+    else {
+        document.getElementById("invalid").innerText= "Wrong Pin";
+    }
+
+    }
+
+
+function deposit() {
+
+    let amount = Number(document.getElementById("amt").value);
+
+    // Checking valid amount
+    if (amount <= 0) {
+        document.getElementById("invalid").innerText= "Invalid Amount";
+        return;
+    }
+
+    // Checking multiple of 100
+    if (amount % 100 !== 0) {
+        document.getElementById("invalid").innerText= "Amount must be multiple of 100";
+        return;
+    }
+
+    // Ask for PIN
+    let enterPin = prompt("Enter PIN");
+
+    if (enterPin == pin) {
+        balance = balance + amount;
+        document.getElementById("totalamount").innerText = "Total Balance : " + balance;
+        alert("Deposit Successful");
+    } 
+    
+    else {
+        document.getElementById("invalid").innerText= "Wrong Pin";
+    }
+}
