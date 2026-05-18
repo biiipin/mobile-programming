6ae00b06bf0bc41c7870a75930ee5e4f6c2c0b61
Bipin Ghimire <gbipin042@gmail.com>
2026-05-18 21:42:50 +0545

    new

--- bankingsystem/script.js
+++ bankingsystem/script.js
@@ -1,72 +1,135 @@
-
 let balance = 10000;
 const pin = "1234";
+
+let balanceVisible = false;
+
+// Show / Hide Balance
+$("#eyeIcon").click(function () {
+
+    if (balanceVisible) {
+
+        document.getElementById("balance").innerText = "******";
+
+        $("#eyeIcon")
+            .removeClass("fa-eye")
+            .addClass("fa-eye-slash");
+
+        balanceVisible = false;
+
+    } else {
+
+        document.getElementById("balance").innerText = balance;
+
+        $("#eyeIcon")
+            .removeClass("fa-eye-slash")
+            .addClass("fa-eye");
+
+        balanceVisible = true;
+    }
+});
+
+// Show Deposit Input
+
+$("#depositBtn").click(function () {
+
+    $("#depositBox").toggle();
+    $("#withdrawBox").hide();
+
+    document.getElementById("invalid").innerText = "";
+});
+
+// Show Withdraw Input
+$("#withdrawBtn").click(function () {
+
+    $("#withdrawBox").toggle();
+    $("#depositBox").hide();
+
+    document.getElementById("invalid").innerText = "";
+});
+
+
+// Connect buttons properly
+$("#confirmDeposit").click(function () {
+    deposit();
+});
+
+$("#confirmWithdraw").click(function () {
+    withdraw();
+});
 
 
 function withdraw() {
 
-    let amount = Number(document.getElementById("amt").value);
+    $("#invalid").text("");
 
-    // Checking valid amount
+    let amount = Number(document.getElementById("num2").value);
+
     if (amount <= 0) {
-        document.getElementById("invalid").innerText= "Invalid Amount";
+        document.getElementById("invalid").innerText = "Invalid Amount";
         return;
     }
 
-    // Checking multiple of 100
     if (amount % 100 !== 0) {
-        document.getElementById("invalid").innerText= "Amount must be multiple of 100";
+        document.getElementById("invalid").innerText = "Amount must be multiple of 100";
         return;
     }
 
-    // Checking sufficient balance
     if (amount > balance) {
-        document.getElementById("invalid").innerText= "Insufficient Balance";
+        document.getElementById("invalid").innerText = "Insufficient Balance";
         return;
     }
 
-    // Asking for pin
     let enterPin = prompt("Enter PIN");
 
     if (enterPin == pin) {
         balance = balance - amount;
-        document.getElementById("totalamount").innerText = "Total Balance : " + balance;
+
+        if (balanceVisible) {
+            document.getElementById("balance").innerText = balance;
+        }
+
+        document.getElementById("num2").value = "";
+
         alert("Withdrawal Successful");
-    } 
-    
-    else {
-        document.getElementById("invalid").innerText= "Wrong Pin";
     }
 
+    else {
+        document.getElementById("invalid").innerText = "Wrong Pin";
     }
+}
 
 
 function deposit() {
 
-    let amount = Number(document.getElementById("amt").value);
+    $("#invalid").text("");
 
-    // Checking valid amount
+    let amount = Number(document.getElementById("num1").value);
+
     if (amount <= 0) {
-        document.getElementById("invalid").innerText= "Invalid Amount";
+        document.getElementById("invalid").innerText = "Invalid Amount";
         return;
     }
 
-    // Checking multiple of 100
     if (amount % 100 !== 0) {
-        document.getElementById("invalid").innerText= "Amount must be multiple of 100";
+        document.getElementById("invalid").innerText = "Amount must be multiple of 100";
         return;
     }
 
-    // Ask for PIN
     let enterPin = prompt("Enter PIN");
 
     if (enterPin == pin) {
         balance = balance + amount;
-        document.getElementById("totalamount").innerText = "Total Balance : " + balance;
+
+        if (balanceVisible) {
+            document.getElementById("balance").innerText = balance;
+        }
+
+        document.getElementById("num1").value = "";
+
         alert("Deposit Successful");
-    } 
-    
+    }
+
     else {
-        document.getElementById("invalid").innerText= "Wrong Pin";
+        document.getElementById("invalid").innerText = "Wrong Pin";
     }
 }
